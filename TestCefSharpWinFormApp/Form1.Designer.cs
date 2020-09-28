abe7d8ac56aa400bc09ef01fffb8a1ac7b9cfe0c
wubuku <yangjiefeng@gmail.com>
2020-09-28 19:33:37 +0800

    修改测试代码。

--- TestCefSharpWinFormApp/Form1.Designer.cs
+++ TestCefSharpWinFormApp/Form1.Designer.cs
@@ -31,18 +31,58 @@ namespace TestCefSharpWinFormApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 409);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(964, 64);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(162, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(964, 473);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
 
 
 
--- TestCefSharpWinFormApp/Form1.cs
+++ TestCefSharpWinFormApp/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using CefSharp;
 using CefSharp.WinForms;
+using System.Net;
 
 namespace TestCefSharpWinFormApp
 {
@@ -29,13 +30,32 @@ namespace TestCefSharpWinFormApp
             chromeBrowser.JavascriptObjectRepository.Register("cefCustomObject", new CefCustomObject(chromeBrowser, this), isAsync: false, options: BindingOptions.DefaultBinder);
         }
 
+        //private string testHtml;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+            
         }
 
         private void InitializeChromium()
         {
-            String page = string.Format(@"{0}\html-resources\html\editor.html", Application.StartupPath);
+            var tempText =
+@"<p>
+<strong>InvokeThread</strong> 解释：
+1。UI执行A
+2。UI开线程InvokeThread，B和C同时执行，B执行在线程UI上，C执行在线程invokeThread上。
+3。invokeThread封送消息给UI，然后自己等待，UI处理完消息后，处理invokeThread封送的消息，即代码段E
+4。UI执行完E后，转到线程invokeThread上，invokeThread线程执行代码段D
+</p>
+";
+            //testHtml = "testcontent";
+            var urlEncodedContent = WebUtility.UrlEncode(tempText);
+            
+            String page = string.Format(@"{0}\html-resources\html\editor.html?content={1}",
+                Application.StartupPath,
+                urlEncodedContent
+                );
             //System.Console.WriteLine(page);
 
             CefSettings settings = new CefSettings();
@@ -53,14 +73,84 @@ namespace TestCefSharpWinFormApp
             browserSettings.FileAccessFromFileUrls = CefState.Enabled;
             browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
             chromeBrowser.BrowserSettings = browserSettings;
-            
+
+
+            //chromeBrowser.RenderProcessMessageHandler = new RenderProcessMessageHandler();
+
+
         }
+
+        //public class RenderProcessMessageHandler : IRenderProcessMessageHandler
+        //{
+        //    // Wait for the underlying `Javascript Context` to be created, this is only called for the main frame.
+        //    // If the page has no javascript, no context will be created.
+        //    void IRenderProcessMessageHandler.OnContextCreated(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+        //    {
+        //        //const string script = "document.addEventListener('DOMContentLoaded', function(){ alert('DomLoaded'); });";
+        //        string script = GetSetHtmlEditorContentScript();
+        //        browserControl.EvaluateScriptAsync(script);
+        //    }
+
+
+        //    public void OnContextReleased(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame)
+        //    {
+        //    }
+
+        //    public void OnFocusedNodeChanged(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IDomNode node)
+        //    {
+        //    }
+
+        //    public void OnUncaughtException(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, JavascriptException exception)
+        //    {
+        //    }
+        //}
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Cef.Shutdown();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string script = string.Format("document.getElementById('startMonth').value;");
+            string script = "$('#editor').froalaEditor('html.get')";
+            this.chromeBrowser.EvaluateScriptAsync(script).ContinueWith(x =>
+            {
+                var response = x.Result;
+
+                if (response.Success && response.Result != null)
+                {
+                    //var startDate = response.Result;
+                    //startDate is the value of a HTML element.
+                    var html = response.Result;
+                    MessageBox.Show(html.ToString());
+                    // 关闭窗口
+                    this.BeginInvoke(new beginInvokeDelegate(this.Close));
+                }
+            });
+        }
+
+        private delegate void beginInvokeDelegate();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string script = GetSetHtmlEditorContentScript();
+            this.chromeBrowser.EvaluateScriptAsync(script).ContinueWith(x =>
+            {
+                var response = x.Result;
+                if (response.Success && response.Result != null)
+                {
+                }
+            });
+        }
+
+        private static string GetSetHtmlEditorContentScript()
+        {
+            string content = "Initialize the Froala <strong>WYSIWYG</strong> HTML 'Editor' on a textarea.";
+            string scriptFormat = "$('#editor').froalaEditor('html.set', '{0}')";
+            string script = String.Format(scriptFormat, content.Replace("'", "\\'"));
+            return script;
+        }
 
     }
 }
